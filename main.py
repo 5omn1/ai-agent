84537352ba43cac0c14dd95d6e0454060743296d
somni <evgengeodakyan@gmail.com>
2026-01-04 21:45:17 -0800

    added --verbose flag

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -13,20 +14,26 @@ def main():
 
     parser = argparse.ArgumentParser(description="Gemini Chatbot")
     parser.add_argument("user_promt", type=str, help="User promt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_promt)])]
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=args.user_promt
+        model="gemini-2.5-flash", contents=messages
     )
     if not response.usage_metadata:
         raise RuntimeError(
             "Error occured during the request to Gemini API for content generation."
         )
-
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+    if args.verbose is True:
+        print(f"User prompt: {args.user_promt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Response:")
+        print(response.text)
+    else:
+        print("Response:")
+        print(response.text)
 
 
 if __name__ == "__main__":
