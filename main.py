c4fe315b28bd330d820080090a834cbcb34972a0
somni <evgengeodakyan@gmail.com>
2026-01-08 23:46:21 -0800

    tells llm which func are available to it

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from prompts import system_prompt
+from functions.call_function import available_functions
 
 def main():
     load_dotenv()
@@ -16,26 +17,34 @@ def main():
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     client = genai.Client(api_key=api_key)
+
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
-        config = types.GenerateContentConfig(system_instruction=system_prompt, temperature=0)
+        config = types.GenerateContentConfig(tools=[available_functions],
+                                             system_instruction=system_prompt,
+                                             temperature=0,
+        ),
     )
+
     if not response.usage_metadata:
         raise RuntimeError(
             "Error occured during the request to Gemini API for content generation."
         )
-    if args.verbose is True:
-        print(f"User prompt: {args.user_promt}")
+
+    if args.verbose:
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if not response.function_calls:
         print("Response:")
         print(response.text)
-    else:
-        print("Response:")
-        print(response.text)
+
+    for func_call in response.function_calls:
+        print(f"Calling function: {func_call.name}({func_call.args})")
 
 
 if __name__ == "__main__":
